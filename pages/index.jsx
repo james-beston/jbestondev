174a24f361e5feca6d212f3050cd97465422a778
James Beston <james@beston.me>
2022-07-03 17:16:38 +0100

    Stylings

---
+++ pages/index.jsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>James Beston - Developer & Designer</title>
+        <meta name="description" content="Welcome to the website fo James Beston - full-stack web designer and designer based in Norfolk, UK." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        
+      </main>
+
+     
+    </div>
+  )
+}
