65a462412d24f7ea77db08b4461364c51ca0cb43
James Beston <james@beston.me>
2022-07-12 09:32:30 +0100

    Schema work

---
+++ backend/schemas/documents/project.js
@@ -0,0 +1,47 @@
+export default {
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'headline_image',
+      title: 'Headline Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'introduction',
+      title: 'Introduction',
+      type: 'text'
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [
+        {
+          type: 'block'
+        },
+        {
+          type: 'image',
+        }
+      ]
+    }
+  ],
+}
